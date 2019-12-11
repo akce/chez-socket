88fcf4eec302075648480dbb4b3f0a8a29e736b6
Akce <>
2019-12-11 13:12:51 +1100

    Reformat with tabs.

--- socket/socket.c
+++ socket/socket.c
@@ -44,94 +44,96 @@ const int c_SHUT_RDWR = SHUT_RDWR;
 
 /* GLIBC: 'man getaddrinfo' for a full C client/server example. */
 
-typedef struct {
-    int socketfd;
-    struct addrinfo* addrs;
-    struct addrinfo* addr;
-} t_connection;
+typedef struct
+	{
+	int socketfd;
+	struct addrinfo* addrs;
+	struct addrinfo* addr;
+	} t_connection;
 
 t_connection*
 make_client_connection(const char* node, const char* service, int ai_family, int ai_socktype, int ai_flags, int ai_protocol)
-    {
-    struct addrinfo hints = {
-        .ai_family = ai_family,
-        .ai_socktype = ai_socktype,
-        .ai_flags = ai_flags,
-        .ai_protocol = ai_protocol
-        };
-    struct addrinfo* addrs = 0;
-    t_connection* conn = 0;
-    int ret = 0;
+	{
+	struct addrinfo hints =
+		{
+		.ai_family = ai_family,
+		.ai_socktype = ai_socktype,
+		.ai_flags = ai_flags,
+		.ai_protocol = ai_protocol
+		};
+	struct addrinfo* addrs = 0;
+	t_connection* conn = 0;
+	int ret = 0;
 
-    ret = getaddrinfo(node, service, &hints, &addrs);
-    if (ret == 0)
-        {
-        /* Success. */
-        struct addrinfo* ptr = 0;
-        int fd = 0;
-        for (ptr = addrs; ptr; ptr = ptr->ai_next)
-            {
-            fd = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-            if (fd == -1)
-                continue;
-            ret = connect(fd, ptr->ai_addr, ptr->ai_addrlen);
-            if (ret == 0)
-                {
-                /* Connected. */
-                conn = malloc(sizeof(*conn));
-                conn->socketfd = fd;
-                conn->addrs = addrs;
-                conn->addr = ptr;
-                break;
-                }
-            else
-                {
-                /* Failed to connect. Close socket and try next address (if any).. */
-                close(fd);
-                }
-            }
-        if (conn == 0)
-            {
-            /* Unable to connect to any address. Cleanup before exit. */
-            freeaddrinfo(addrs);
-            /* TODO propagate error to scheme code.. */
-            }
-        }
-    else
-        {
-        /* TODO propagate error to scheme code.. */
-        printf("%d: %s\n", ret, gai_strerror(ret));
-        }
+	ret = getaddrinfo(node, service, &hints, &addrs);
+	if (ret == 0)
+		{
+		/* Success. */
+		struct addrinfo* ptr = 0;
+		int fd = 0;
+		for (ptr = addrs; ptr; ptr = ptr->ai_next)
+			{
+			fd = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+			if (fd == -1)
+				continue;
+			ret = connect(fd, ptr->ai_addr, ptr->ai_addrlen);
+			if (ret == 0)
+				{
+				/* Connected. */
+				conn = malloc(sizeof(*conn));
+				conn->socketfd = fd;
+				conn->addrs = addrs;
+				conn->addr = ptr;
+				break;
+				}
+			else
+				{
+				/* Failed to connect. Close socket and try next address (if any).. */
+				close(fd);
+				}
+			}
+		if (conn == 0)
+			{
+			/* Unable to connect to any address. Cleanup before exit. */
+			freeaddrinfo(addrs);
+			/* TODO propagate error to scheme code.. */
+			}
+		}
+	else
+		{
+		/* TODO propagate error to scheme code.. */
+		printf("%d: %s\n", ret, gai_strerror(ret));
+		}
 
-    return conn;
-    }
+	return conn;
+	}
 
 /* GLIBC: 'man 2 close' */
 void
 connection_close(t_connection* conn)
-    {
-    if (conn && (conn->socketfd != -1))
-        {
-        if (close(conn->socketfd) == 0)
-            {
-            conn->socketfd = -1;
-            freeaddrinfo(conn->addrs);
-            }
-        /* TODO propagate error to scheme code.. */
-        }
-    }
+	{
+	if (conn && (conn->socketfd != -1))
+		{
+		if (close(conn->socketfd) == 0)
+			{
+			conn->socketfd = -1;
+			freeaddrinfo(conn->addrs);
+			}
+		/* TODO propagate error to scheme code.. */
+		}
+	}
 
 /* GLIBC: 'man 2 shutdown' */
 void
 connection_shutdown(t_connection* conn, int how)
-    {
-    if (conn && (conn->socketfd != -1))
-        {
-        /* TODO assumes 'how' is a valid value. */
-        if (shutdown(conn->socketfd, how) != 0)
-            {
-            /* TODO propagate error to scheme code.. */
-            }
-        }
-    }
+	{
+	if (conn && (conn->socketfd != -1))
+		{
+		/* TODO assumes 'how' is a valid value. */
+		if (shutdown(conn->socketfd, how) != 0)
+			{
+			/* TODO propagate error to scheme code.. */
+			}
+		}
+	}
 
